f96f69883c388546f2063fbaeb997593eabb74cd
Toanf2103 <nobitakute002@gmail.com>
2022-10-05 09:07:46 +0700

    first commit

--- app/src/main/java/com/example/coffeapp/MainActivity.java
+++ app/src/main/java/com/example/coffeapp/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.coffeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtSinup;
@@ -20,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
     EditText edtEmail;
     EditText edtPass;
     TextView tvDangKi;
+    CheckBox cbNho;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("dataLogin",MODE_PRIVATE);
         anhXa();
+        edtEmail.setText(sharedPreferences.getString("email",""));
+        edtPass.setText(sharedPreferences.getString("pass",""));
+        cbNho.setChecked(sharedPreferences.getBoolean("nho",false));
+
+
         quaDangKi();
         login();
         hiddenPass();
@@ -35,10 +46,15 @@ public class MainActivity extends AppCompatActivity {
         edtPass=(EditText) findViewById(R.id.edtPass);
         tvDangKi =(TextView) findViewById(R.id.btnDangKi);
         btnLogin =(Button) findViewById(R.id.loginBtn);
+        cbNho=(CheckBox) findViewById(R.id.cbNho);
+
     }
 
     private void quaDangKi(){
         tvDangKi.setOnClickListener(v -> {
+
+
+
             Intent i = new Intent(MainActivity.this,Singin.class);
             startActivity(i);
         });
@@ -65,8 +81,35 @@ public class MainActivity extends AppCompatActivity {
     }
     private  void login(){
         btnLogin.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this,index.class);
-            startActivity(i);
+
+
+            String email = edtEmail.getText().toString();
+            String pass = edtPass.getText().toString();
+            if (email.equals("")||pass.equals("")){
+                Toast.makeText(this, "Nhập tk mk bạn ơi", Toast.LENGTH_SHORT).show();
+            }else{
+                if(cbNho.isChecked()){
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.putString("email",email);
+                    editor.putString("pass",pass);
+                    editor.putBoolean("nho",true);
+                    editor.commit();
+                    Toast.makeText(this, "Lưu rồi", Toast.LENGTH_SHORT).show();
+                }else{
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.remove("email");
+                    editor.remove("pass");
+                    editor.remove("nho");
+                    Toast.makeText(this, "Quên rồi", Toast.LENGTH_SHORT).show();
+                }
+
+                Intent i = new Intent(MainActivity.this,index.class);
+                ;
+                startActivity(i);
+            }
+
         });
     }
 }
