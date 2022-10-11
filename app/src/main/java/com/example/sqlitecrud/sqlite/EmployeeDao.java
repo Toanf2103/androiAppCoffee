a97c80d9a96f47f5e7a4390111af9ce04f2eb79e
Toanf2103 <nobitakute002@gmail.com>
2022-10-11 22:46:21 +0700

    done Dao

--- app/src/main/java/com/example/sqlitecrud/sqlite/EmployeeDao.java
+++ app/src/main/java/com/example/sqlitecrud/sqlite/EmployeeDao.java
@@ -1,6 +1,8 @@
 package com.example.sqlitecrud.sqlite;
 
 import android.annotation.SuppressLint;
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -30,5 +32,33 @@ public class EmployeeDao {
             list.add(emp);
         }
         return  list;
+    }
+    public List<Employee> getAll(){
+        String sql = "SELECT * from SinhVien";
+         return get(sql);
+    }
+    public Employee getByMsv(String msv){
+        String sql ="SELECT *from SinhVien Where msv = ?";
+        List<Employee> list = get(sql,msv);
+        return  list.get(0);
+    }
+    public long insert(Employee sv){
+        ContentValues values = new ContentValues();
+        values.put("msv",sv.getMsv());
+        values.put("ten",sv.getTen());
+        values.put("namSinh",sv.getNamSinh());
+
+        return db.insert("SinhVien",null,values);
+    }
+    public long update(Employee sv){
+        ContentValues values = new ContentValues();
+
+        values.put("ten",sv.getTen());
+        values.put("namSinh",sv.getNamSinh());
+
+        return db.update("SinhVien",values,"id=?",new String[]{sv.getMsv()});
+    }
+    public int delete(String id){
+        return db.delete("SinhVien","id=?",new String[]{id});
     }
 }
